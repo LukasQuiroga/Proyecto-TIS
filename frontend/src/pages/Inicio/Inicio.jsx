1a53f3ae1db14ae99bfff049e46815aea547f150
PanaShusco <joelllusco1@gmail.com>
2026-09-20 18:17:33 -0400

    refactor: separar estilos de la interfaz principal

--- frontend/src/pages/Inicio/Inicio.jsx
+++ frontend/src/pages/Inicio/Inicio.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../../components/Navbar/Navbar";
 import Sidebar from "../../components/Sidebar/Sidebar";
+import "../../components/Layout/PlantillaPrincipal.css";
 import "./Inicio.css";
 
 function IconoModulo({ tipo }) {
