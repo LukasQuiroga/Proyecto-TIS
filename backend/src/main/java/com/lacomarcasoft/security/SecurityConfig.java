bfa6e728ae650c09ca205b2ace88375bcc1b7106
AmirVidal <amirvidal2004@gmail.com>
2026-09-25 01:40:41 -0400

    Arreglos DataBase

--- backend/src/main/java/com/lacomarcasoft/security/SecurityConfig.java
+++ backend/src/main/java/com/lacomarcasoft/security/SecurityConfig.java
@@ -9,8 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -57,10 +55,7 @@ public class SecurityConfig {
         return new InMemoryUserDetailsManager(admin);
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
+    
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
